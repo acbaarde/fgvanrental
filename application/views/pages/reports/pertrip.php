61a2cf6f4cb226e0442fab36e3a68c6612e74446
SPARE\admin <acbaarde@sl-agritech.com>
2025-01-21 13:51:03 +0800

    use temporary report header

--- application/views/pages/reports/pertrip.php
+++ application/views/pages/reports/pertrip.php
@@ -26,11 +26,17 @@
             <div class="card-body">
                 <div class="form-group row" style="margin-bottom: 2rem;">
                     <div class="col-md-12">
-                        <img src="../assets/images/logogv.jpg" alt="logogv.jpg" style="position: absolute; float: inline-start; width:140px; height:140px; margin-left: 50px;">
+                        <!-- <img src="../assets/images/logogv.jpg" alt="logogv.jpg" style="position: absolute; float: inline-start; width:140px; height:140px; margin-left: 50px;">
                         <ul class="list-unstyled" style="text-align: center; font-size: medium; margin-top: 1rem;">
                             <li><h5><strong><?php echo $sysinfo['name']; ?></strong></h5></li>
                             <li><h6><strong><?php echo $sysinfo['address']; ?></strong></h6></li>
                             <li><h6><strong>Contact No. <?php echo $sysinfo['contact']; ?></strong></h6></li>
+                        </ul> -->
+
+                        <img src="../assets/images/logofg.png" alt="logogv.jpg" style="display: block; margin-left: auto; margin-right: auto; width:500px; height:100px;">
+                        <ul class="list-unstyled" style="text-align: center; font-size: medium; margin-top: 10px;">
+                            <li>Blk 6 Lot 34 A Area F Brgy. San Pedro Sapang Palay City of SJDM, Bulacan</li>
+                            <li>Contact No. 0926-0581-888 / 0927-3079-766</li>
                         </ul>
                     </div>
                 </div>
