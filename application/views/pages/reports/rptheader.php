2c1fbfebb5131818bd2bf5a934d1d7bf69dab742
SPARE\admin <acbaarde@sl-agritech.com>
2025-03-07 14:22:24 +0800

    change company name

--- application/views/pages/reports/rptheader.php
+++ application/views/pages/reports/rptheader.php
@@ -1,4 +1,4 @@
-<!-- <div class="form-group row" style="margin-bottom: 2rem;">
+<div class="form-group row" style="margin-bottom: 2rem;">
     <div class="col-md-12">
         <img src="../../assets/images/logogv.jpg" alt="logogv.jpg" style="position: absolute; float: inline-start; width:140px; height:140px; margin-left: 50px;">
         <ul class="list-unstyled" style="text-align: center; font-size: medium; margin-top: 1rem;">
@@ -7,9 +7,9 @@
             <li><h6><strong>Contact No. <?php echo $sysinfo['contact']; ?></strong></h6></li>
         </ul>
     </div>
-</div> -->
+</div>
 
-<div class="form-group row" style="margin-bottom: 1rem;">
+<!-- <div class="form-group row" style="margin-bottom: 1rem;">
     <div class="col-md-12">
         <img src="../../assets/images/logofg.png" alt="logogv.jpg" style="display: block; margin-left: auto; margin-right: auto; width:500px; height:100px;">
         <ul class="list-unstyled" style="text-align: center; font-size: medium; margin-top: 10px;">
@@ -17,4 +17,4 @@
             <li>Contact No. 0926-0581-888 / 0927-3079-766</li>
         </ul>
     </div>
-</div>
+</div> -->
